ec70a91cce62a2a199e56b396be62afdf786a5fc
Nikita Sinha <nikitasinha@EtherLabss-MacBook-Pro.local>
2020-07-27 15:54:07 +0530

    Added design to the card

--- src/Components/LandingPage.jsx
+++ src/Components/LandingPage.jsx
@@ -1,61 +1,41 @@
 import React, { Component } from 'react';
-import { Card, Modal, DatePicker } from 'antd';
+import { Card, Modal, Button } from 'antd';
 import dateFormat from 'dateformat';
 import 'antd/dist/antd.css';
 import './LandingPage.css';
-
+import "react-datepicker/dist/react-datepicker.css";
 
 class LandingPage extends Component {
     constructor() {
         super()
         this.state = {
             userDetails: [],
+            selectedUserDetails: [],
             visible: false,
-            pickerOpen: false,
-            selectedDate: null,
+            visibleNested: false,
             curTime: new Date(),
-            trialArray: [ ],
-            activeItemId: null
-
+            selectedDate: new Date(),
         }
+        this.handleChange = this.handleChange.bind(this);
 
     }
     componentDidMount() {
-        var pushedArray = []
-        const convertedDateFormat = dateFormat(this.state.curTime, "mmmm dd yyyy");
+
 
         fetch('data/file.json')
             .then((response) => response.json())
             .then((data) => {
                 this.setState({
                     userDetails: data.members
-                })
-                console.log(this.state.userDetails)
+                });
             })
-           
-        // this.state.trialArray.map((item) => {
-        //     const newdatee = item.activity_periods[3].start_time
-        //     var res = newdatee.split(" ");  //split by space
-        //     res.pop();  //remove last elemen
-        //     const convertedStartTime = res.join(" ")
-        //     console.log(convertedStartTime)
-        //     const convertedDateFormat = dateFormat(this.state.curTime, "mmmm dd yyyy");
-        //        if(convertedDateFormat === convertedStartTime){
-        //            console.log('h')
-        //        }
-        //        else{
-        //            console.log(convertedDateFormat,convertedStartTime)
-        //        }
-
-        // })
-
-
     }
 
-    showModal = () => {
-        
+    showModal = (id, e) => {
+
         this.setState({
-            visible: true
+            visible: true,
+            selectedUserDetails: this.state.userDetails.filter((x) => x.id === id),
         });
     };
 
@@ -63,6 +43,7 @@ class LandingPage extends Component {
         console.log(e);
         this.setState({
             visible: false,
+            visibleNested: false
         });
     };
 
@@ -70,33 +51,115 @@ class LandingPage extends Component {
         console.log(e);
         this.setState({
             visible: false,
+            visibleNested: false
+        });
+    };
+   
+    handleChange(event) {
+        this.setState({
+            selectedDate: event.target.value
+        });
+    }
+    showNestedModal = () => {
+        this.setState({
+            visibleNested: true
         });
     };
 
 
     render() {
-        const { userDetails, trialArray } = this.state;
-        const dateFormatting = 'YYYY/DD/MM';
-        console.log(this.state.activeItemId)
-
+        const { userDetails, selectedDate, curTime } = this.state;
+        const convertedPresentDate = dateFormat(curTime, "mmm dd yyyy");
+        const convertedSelectedDate = dateFormat(selectedDate, "mmm dd yyyy");
+        console.log(this.state.selectedUserDetails)
         return (
-            <Card title="List of Users" style={{ width: 400, height: 400 }}>
+            <Card title="LIST OF USERS" style={{ width: 400, height: 400 }}>
                 {
                     userDetails.map((item, index) => {
                         return (
                             <ul key={index}>
-                                <li onClick={this.showModal}>{item.real_name}</li>
+                                <li onClick={this.showModal.bind(this, item.id)}>{item.real_name}</li>
                                 <Modal
-                                    title="Today's activities"
+                                    title="Activity log"
                                     visible={this.state.visible}
                                     onOk={this.handleOk}
                                     onCancel={this.handleCancel}
+                                    destroyOnClose={true}
                                 >
-                                    <p>Some contents...</p>
-                                    <p>Some contents...</p>
-                                    <p>Some contents...</p>
-                                    <DatePicker format={dateFormatting} />
+                                    {
+                                        this.state.selectedUserDetails.map((item, index) => {
 
+                                            return (
+                                                <div key={index}>
+                                                    {item.activity_periods.map((c, i) => {
+                                                        const startTime = c.start_time;
+                                                        const endTime = c.end_time;
+                                                        var res = startTime.split(" ");  //split by space
+                                                        res.pop();  //
+                                                        const convertedUserDate = res.join(" ");
+                                                        const userDateValue = new Date(convertedUserDate)
+                                                        const presentDateValue = new Date(convertedPresentDate)
+                                                        if (userDateValue.valueOf() === presentDateValue.valueOf()) {
+                                                            return (
+                                                                <div key={i}>
+                                                                    <h2>Today's activities</h2>
+                                                                    <h4>{startTime}</h4>
+                                                                    <h4>{endTime}</h4>
+                                                                </div>
+                                                                
+                                                            )
+                                                        }
+                                                    }
+                                                    )}
+                                                </div>
+                                            )
+                                        })
+                                    }
+                                   
+                                    <div className="wrapper">
+                                        <p>To view activities, enter/select date below.</p>
+                                        <input
+                                            type="date"
+                                            name="startDate"
+                                            value={this.state.selectedDate}
+                                            onChange={event => this.setState({ selectedDate: event.target.value })}
+                                        />
+                                        <Button type="primary" onClick={this.showNestedModal}>
+                                            Search log
+                                    </Button>
+                                    </div>
+                                    <Modal
+                                        title="Activity log"
+                                        visible={this.state.visibleNested}
+                                        onOk={this.handleOk}
+                                        onCancel={this.handleCancel}
+                                      
+                                    >
+                                        {
+                                            this.state.selectedUserDetails.map((item, index) => {
+                                                return (
+                                                    <div key={index}>
+                                                        {item.activity_periods.map((item, index) => {
+                                                            const startTime = item.start_time;
+                                                            var res = startTime.split(" ");
+                                                            res.pop();
+                                                            const convertedUserDate = res.join(" ");
+                                                            const userValue = new Date(convertedUserDate)
+                                                            const selectedValue = new Date(convertedSelectedDate)
+                                                            if (userValue.valueOf() === selectedValue.valueOf()) {
+                                                                return (
+                                                                    <div key={index}>
+                                                                        <h4>{startTime}</h4>
+                                                                    </div>
+                                                                )
+                                                            }
+                                                        }
+                                                        )}
+                                                    </div>
+                                                )
+                                            })
+                                        }
+                                    </Modal>
                                 </Modal>
                             </ul>
                         )
